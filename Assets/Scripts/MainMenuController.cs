dff2a459f2478126df7ad75f678560a6432c1cb1
Bilal <bilalshaikh370@gmail.com>
2020-03-11 15:50:37 -0400

    Start&End  Scene Added

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+         SceneManager.LoadScene("Level1");
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);    
+    }
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+
+
+
+
+    }
+public void GameOver()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
