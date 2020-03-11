945dce1defbd19e47a3d8c57e56e33217fbe7d20
Bilal <bilalshaikh370@gmail.com>
2020-03-11 13:42:36 -0400

    Obstacles Added

---
+++ Assets/Scripts/ObstacleTouch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ObstacleTouch : MonoBehaviour
+{
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene("EndScene");
+        }
+    }
+
+}
